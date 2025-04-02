4616f91862417fde620932aa12415e793891afbe
berluk12@gmail.com <berluk12@gmail.com>
2025-04-02 10:21:39 +0200

    ENGLRN-65849 Know words reviewing

--- src/main/java/com/bervan/englishtextstats/view/AbstractNotLearnedWordsBaseView.java
+++ src/main/java/com/bervan/englishtextstats/view/AbstractNotLearnedWordsBaseView.java
@@ -7,6 +7,7 @@ import com.bervan.core.model.BervanLogger;
 import com.bervan.englishtextstats.Word;
 import com.bervan.englishtextstats.service.WordService;
 import com.bervan.languageapp.service.AddAsFlashcardService;
+import com.vaadin.flow.component.ClientCallable;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.grid.Grid;
@@ -21,6 +22,8 @@ import java.util.UUID;
 public abstract class AbstractNotLearnedWordsBaseView extends AbstractTableView<UUID, Word> {
     protected HorizontalLayout dialogButtonsLayout;
     protected final AddAsFlashcardService addAsFlashcardService;
+    protected Button addAsFlashcard;
+    protected Button markAsLearned;
 
     public AbstractNotLearnedWordsBaseView(WordService service, BervanLogger log, MenuNavigationComponent layout, AddAsFlashcardService addAsFlashcardService) {
         super(layout, service, log, Word.class);
@@ -46,6 +49,11 @@ public abstract class AbstractNotLearnedWordsBaseView extends AbstractTableView<
 
     @Override
     protected void buildOnColumnClickDialogContent(Dialog dialog, VerticalLayout dialogLayout, HorizontalLayout headerLayout, String clickedColumn, Word item) {
+        dialog.addDetachListener(e -> {
+            markAsLearned = null;
+            addAsFlashcard = null;
+        });
+
         dialogButtonsLayout = new HorizontalLayout();
 
         TextArea field = new TextArea(clickedColumn);
@@ -53,20 +61,18 @@ public abstract class AbstractNotLearnedWordsBaseView extends AbstractTableView<
 
         field.setValue(item.getTableFilterableColumnValue());
 
-        Button saveButton = new BervanButton("Mark as learned.");
+        markAsLearned = new BervanButton("Mark as learned '['");
 
-        saveButton.addClickListener(e -> {
+        markAsLearned.addClickListener(e -> {
             data.remove(item);
             grid.getDataProvider().refreshAll();
             service.save(item);
             dialog.close();
         });
 
-        dialogButtonsLayout.add(saveButton);
-
         dialogLayout.add(headerLayout, field, dialogButtonsLayout);
 
-        Button addAsFlashcard = new Button("Add as flashcard.");
+        addAsFlashcard = new BervanButton("Add as flashcard ']'");
         addAsFlashcard.addClickListener(e -> {
             data.remove(item);
             grid.getDataProvider().refreshAll();
@@ -76,7 +82,35 @@ public abstract class AbstractNotLearnedWordsBaseView extends AbstractTableView<
             dialog.close();
         });
 
+        dialogButtonsLayout.add(markAsLearned);
         dialogButtonsLayout.add(addAsFlashcard);
+
+
+        getElement().executeJs(
+                " document.addEventListener('keydown', function(event) {" +
+                        "    if (event.key === '[') {" +
+                        "        $0.$server.markAsLearned() " +
+                        "    } else if (event.key === ']') {" +
+                        "        $0.$server.addAsFlashcard() " +
+                        "    } else {return;}" +
+                        " }); "
+                ,
+                getElement()    // $0
+        );
+    }
+
+    @ClientCallable
+    public void markAsLearned() {
+        if (markAsLearned != null) {
+            markAsLearned.click();
+        }
+    }
+
+    @ClientCallable
+    public void addAsFlashcard() {
+        if (addAsFlashcard != null) {
+            addAsFlashcard.click();
+        }
     }
 
     @Override
