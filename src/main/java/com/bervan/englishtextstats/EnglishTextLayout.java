c126ca1135c22a9b90272c0faf8036505920829f
berluk12@gmail.com <berluk12@gmail.com>
2025-08-17 14:27:17 +0200

    New Navigation menu styling

--- src/main/java/com/bervan/englishtextstats/EnglishTextLayout.java
+++ src/main/java/com/bervan/englishtextstats/EnglishTextLayout.java
@@ -3,13 +3,14 @@ package com.bervan.englishtextstats;
 import com.bervan.common.MenuNavigationComponent;
 import com.bervan.englishtextstats.view.AbstractEbooksView;
 import com.bervan.englishtextstats.view.AbstractNotLearnedWordsView;
+import com.vaadin.flow.component.icon.VaadinIcon;
 
 public class EnglishTextLayout extends MenuNavigationComponent {
     public EnglishTextLayout(String routeName) {
         super(routeName);
 
-        addButtonIfVisible(menuButtonsRow, AbstractNotLearnedWordsView.ROUTE_NAME, "Not Learned Words");
-        addButtonIfVisible(menuButtonsRow, AbstractEbooksView.ROUTE_NAME, "Ebooks");
+        addButtonIfVisible(menuButtonsRow, AbstractNotLearnedWordsView.ROUTE_NAME, "Not Learned Words", VaadinIcon.HOME.create());
+        addButtonIfVisible(menuButtonsRow, AbstractEbooksView.ROUTE_NAME, "Ebooks", VaadinIcon.HOME.create());
 
         add(menuButtonsRow);
     }
